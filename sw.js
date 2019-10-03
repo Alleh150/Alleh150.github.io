632d5972326c5724d877ef7e726c37fb285e376b
Alleh150 <55537702+Alleh150@users.noreply.github.com>
2019-10-03 08:43:15 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-self.addEventlistener('install' , evt => {console.log('Service worker foi instalado.');
+self.addEventlistener('install' , evt => {
+	console.log('Service worker foi instalado.');
 });
 
-self.addEventlistener('activate', evt =>
-{
-console.log('Service worker foi ativado.');
+self.addEventlistener('activate', evt =>{
+	console.log('Service worker foi ativado.');
 });
 
 self.addEventlistener('fetch' , evt => {
