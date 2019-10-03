8826a05350b7c320f1c669e04696d868a115bbb3
Alleh150 <55537702+Alleh150@users.noreply.github.com>
2019-10-03 08:18:22 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventlistener('install' , evt => {console.log('Service worker foi instalado.');
+});
+
+self.addEventlistener('activate', evt =>
+{
+console.log('Service worker foi ativado.');
+});
+
+self.addEventlistener('fetch' , evt => {
+	 console.log('Service worker capturou um evento do tipo fetch.');
+});
