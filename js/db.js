3b7a773c904bb47a02026a048ee57855a261fb39
Alleh150 <55537702+Alleh150@users.noreply.github.com>
2019-11-14 08:11:43 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -7,7 +7,7 @@ db.enablePersistence()
         }
     });
 
-db.collection('Tecnico').onSnapshot(snapshot => {
+db.collection('Friendly Cat').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
             renderRecipe(change.doc.data(), change.doc.id);
