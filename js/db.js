8e33b94c71a49a322f78735f4debe9567c56bc8f
Alleh150 <55537702+Alleh150@users.noreply.github.com>
2019-11-21 08:50:05 -0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,18 +1,46 @@
+// habilitar dados offline
+
 db.enablePersistence()
     .catch(function(err) {
         if (err.code == 'failed-precondition') {
-            console.log('persistance failed');
+            // provavelmente multiplas abas abertas ao mesmo tempo
+            console.log('Persistencia de dados falhou');
         } else if (err.code == 'unimplemented') {
-            console.log('persistance not available');
+             // browser nao suporta
+            console.log('Persistencia nao disponivel');
         }
     });
-
+// real-time listener que verifica as mudanças que ocorrem
 db.collection('Friendly Cat').onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
         if (change.type === 'added') {
-            renderRecipe(change.doc.data(), change.doc.id);
+             desenhaCard(change.doc.data(), change.doc.id);
         }
         if (change.type === 'removed') {
+            // remover da pagina tambem
         }
     });
+});
+
+// adicionar novo produto
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+    evt.preventDefault();
+
+    const produto = {
+        nome: form.NomeProduto.value,
+        descricao: form.DescricaoProduto.value,
+        link: form.produtoLink.value,
+        arquivo: form.produtoArquivo.value
+    };
+
+    db.collection('produto').add(produto)
+        .catch(err => console.log(err));
+
+    //reseta o formulario
+    form.NomeProduto.value = '';
+    form.DescricaoProduto.value = '';
+    form.produtoLink.value = '';
+    form.produtoArquivo.value = '';
+
 });
