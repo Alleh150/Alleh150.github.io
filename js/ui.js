d4423a7adc12f33401d1226c60803062b34e1a91
Alleh150 <55537702+Alleh150@users.noreply.github.com>
2019-11-21 08:41:23 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,40 @@
+// essa funcao seleciona o row (que esta no index)
+// para depois desenhar todos os cards la
+const produto = document.querySelector('.produto');
+
 document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('.sidenav');
-	var instances = M.Sidenav.init(elems);
+    // menus laterais 
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge:'left'});
+
+    // formulario para adicionar produto
+    const forms = document.querySelectorAll('.side-form');
+    var instances2 = M.Sidenav.init(forms, {edge:'right'});
 });
+
+//funcao para desenhar o card da receita na tela
+//id, nome, descricao, link, arquivo
+const desenhaCard = (data, id) => {
+
+	const html = `
+  <div class="col s12 m6 l3 produto" data-id="${id}">
+        <div class="card" style="background-color: #A59C94FF;">
+            <div class="card-image produto-imagem">
+                <img src="images/${data.arquivo}">
+                <span class="card-title nome-produto"${data.nome}"</span>
+            </div>
+            <div class="card-content produto-descricao">
+                <p>"${data.descricao}"</p>
+            </div>
+            <div class="card-action">
+                <a style="color: #AE0E36FF;" class="produto-link" href="${data.link}">Saiba mais</a>
+            </div>
+            <div class="produto-deletar">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+        </div>
+   </div>`;
+    produto.innerHTML += html;
+
+};
+
